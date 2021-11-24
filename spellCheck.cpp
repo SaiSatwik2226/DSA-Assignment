2ed67e9fda400204f9a99bcc1033da58a180c6d1
Sai Prasoona <62608612+prasoona416@users.noreply.github.com>
2021-11-24 16:32:39 +0530

    Update spellCheck.cpp

--- spellCheck.cpp
+++ spellCheck.cpp
@@ -1,5 +1,29 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
 
-int main(){
-    
+bool
+spellCheck (const unordered_set <string> &dictionary, const string & word)
+{
+  return dictionary.count (word) != 0;
+}
+
+int main ()
+{
+  ifstream in;
+  in.open("dictionary.txt");
+  string line;
+  vector<string> myvector;
+  if(in.is_open()){
+      while(!in.eof()){
+          getline(in,line);
+          myvector.push_back(line);
+      }
+      in.close();
+  }
+ unordered_set<string> dict(myvector.begin(),myvector.end());
+  
 }

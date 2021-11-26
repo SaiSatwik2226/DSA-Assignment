0a4c5d08c5cc62ece72e0485a7e54f2069af7c32
Sai Prasoona <62608612+prasoona416@users.noreply.github.com>
2021-11-26 16:23:57 +0530

    Update spellCheck.cpp

--- spellCheck.cpp
+++ spellCheck.cpp
@@ -11,19 +11,37 @@ spellCheck (const unordered_set <string> &dictionary, const string & word)
   return dictionary.count (word) != 0;
 }
 
+vector<string> spellCheck(const string &str,const unordered_set<string> &dictionary){
+    vector<string> wrong_words;
+    stringstream ss(str);
+    string word;
+    while(ss>>word){
+        if(!checkSpell(dictionary,word)){
+            wrong_words.push_back(word);
+        }
+    }
+    return wrong_words;
+}
+
 int main ()
 {
-  ifstream in;
-  in.open("dictionary.txt");
-  string line;
-  vector<string> myvector;
-  if(in.is_open()){
-      while(!in.eof()){
-          getline(in,line);
-          myvector.push_back(line);
-      }
-      in.close();
-  }
- unordered_set<string> dict(myvector.begin(),myvector.end());
+  string str="act actd acting";
+    ifstream in;
+    in.open("Dictionary.txt");
+    string line;
+    vector<string> myvector;
+    if(in.is_open()){
+        while(!in.eof()){
+            getline(in,line);
+            myvector.push_back(line);
+        }
+        in.close();
+    }
+    unordered_set<string> dict(myvector.begin(),myvector.end());
+    vector<string> ans = spellCheck(str,dict);
+    vector<string>::iterator it;
+    for(it=ans.begin();it!=ans.end();++it){
+        cout<<*it<<endl;
+    }
   
 }
